5bc03bac099b2482e1d2718e3d653440586817c0
Jon <jbvaldesuso@gmail.com>
2018-06-14 17:30:46 -0400

    Show Create is OK

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -8,36 +8,47 @@ class Api::V1::ShowsController < ApplicationController
     shows.each {|show| newAll.push({show: show, bands: show.bands})}
     render json: newAll, status: 200
   end
-  #
-  # def create
-  #   show = Show.create(show_params)
-  #   render json: show, status: 201
-  # end
-  #
-  #
-  # def update
-  #   @show.update(show_params)
-  #   render json: @show, status: 200
-  # end
-  #
-  # def destroy
-  #    showId = @show.id
-  #    @show.destroy
-  #    render json: {message: "Show deleted!", showId: showId}
-  # end
-  #
-  # def show
-  #   # render json: @show, status: 200
-  # end
-  #
-  # private
-  #
-  # def show_params
-  #   params.require(:show).permit(:name, :flyer)
-  # end
-  #
-  # def set_show
-  #   @show = Show.find(params[:id])
-  # end
+
+  def create
+    @show = Show.new
+    @show.name = params[:name]
+    @show.flyer = params[:flyer]
+    @show.venue_id = params[:venue_id]
+    @show.band_ids = params[:band_ids]
+    if (@show.save)
+      render json: @show
+    else
+      render json: {
+        errors: @show.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+
+
+  def update
+    @show.update(show_params)
+    render json: @show, status: 200
+  end
+
+  def destroy
+     showId = @show.id
+     @show.destroy
+     render json: {message: "Show deleted!", showId: showId}
+  end
+
+  def show
+    newShow = {show: @show, bands: @show.bands}
+    render json: newShow, status: 200
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:name, :flyer, :venue_id, :band_ids)
+  end
+
+  def set_show
+    @show = Show.find(params[:id])
+  end
 
 end
